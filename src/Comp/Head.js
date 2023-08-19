19bf30e6a6f15339dfbf697e3a3041cd1874cda0
NeoDev <you@example.com>
2023-08-18 21:33:03 -0500

    the problem str to bool in themeProvider

--- src/Comp/Head.js
+++ src/Comp/Head.js
@@ -1,16 +1,16 @@
 //https://react-icons.github.io/react-icons
 import { useContext } from "react";
-import { BiSun } from "react-icons/bi";
+import { BiSun, BiMoon } from "react-icons/bi";
 import { ThemeContext } from "../Context/ThemeProvider";
 
 const Head = () => {
-  const {isDark} = useContext(ThemeContext)
+  const {isDark, dispatch} = useContext(ThemeContext)
   console.log(isDark)
   return (
     <header>
       <h1>TODO</h1>
-      <button className="btn btn-round">
-        <BiSun />
+      <button className="btn btn-round" onClick={() => dispatch({type: "toggleTheme"})}>
+        {isDark ? <BiMoon /> : <BiSun />}
       </button>
     </header>
   )
--- src/Context/ThemeProvider.js
+++ src/Context/ThemeProvider.js
@@ -1,18 +1,34 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
 const ThemeReducer = (state, action) => { 
   switch (action.type) {
+    case 'toggleTheme':
+      return !state
     default:
       return state;
   }
 }
 
 export const ThemeProvider = ({children}) => { 
-  const [isDark, dispatch] = useReducer(ThemeReducer, false)
+  const [isDark, dispatch] = useReducer(ThemeReducer, null, ()=>{
+    const LS_theme = localStorage.getItem('theme')
+    return LS_theme ? Boolean(LS_theme) : false;
+  })
+  
+  useEffect(() => {
+    localStorage.setItem('theme', isDark.toString())
+    if(isDark){
+      document.querySelector('.App').classList.add('dark')
+    }
+    else{
+      document.querySelector('.App').classList.remove('dark')
+    }
+  }, [isDark])
+
   return (
-    <ThemeContext.Provider value={{isDark}}>
+    <ThemeContext.Provider value={{isDark, dispatch}}>
       {children}
     </ThemeContext.Provider>
   )
