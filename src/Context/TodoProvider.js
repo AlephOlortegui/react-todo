d783ce693d71d71a39a9a6a986e81784d702c456
NeoDev <you@example.com>
2023-08-19 10:56:04 -0500

    todoProvider added LS to our todos

--- src/Context/TodoProvider.js
+++ src/Context/TodoProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react"
 import TodoReducer from "../reducer/TodoReducer"
 
 const initialState = {
@@ -11,7 +11,18 @@ const initialState = {
 export const TodoContext = createContext()
 
 export const TodoProvider = ({children}) => { 
-  const [state, dispatch] = useReducer(TodoReducer, initialState)
+  const [state, dispatch] = useReducer(TodoReducer, initialState, () =>{
+    const storedTodos = localStorage.getItem('todos')
+    return {
+      ...initialState,
+      todos: storedTodos ? JSON.parse(storedTodos) : initialState.todos
+    }
+  })
+
+   // Use useEffect to save 'todos' to localStorage whenever it changes
+   useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(state.todos));
+  }, [state.todos]);
 
   return (
     <TodoContext.Provider value={{
